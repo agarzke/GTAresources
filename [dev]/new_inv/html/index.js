376c91475b0004f6a2fb4021e5364b61387f459a
Alex <alex.garzke@hotmail.de>
2021-08-26 02:05:22 +0200

    develop game changer

--- [dev]/new_inv/html/index.js
+++ [dev]/new_inv/html/index.js
@@ -21,6 +21,7 @@ $(function(){
             $("#inventory").hide();
         }
     }
+    // big gamechanger in develop
     display(false) // DEBUG! Set to false!
 
     window.addEventListener("message", function(event){
@@ -36,7 +37,7 @@ $(function(){
         }
     })
 
-    document.onkeyup = function(data){                  // ESC press: 
+    document.onkeyup = function(data){                  // ESC press:
         if(data.which == 27){
             $.post("http://new_inv/exit", JSON.stringify({}));
             return;
@@ -51,11 +52,11 @@ $(function(){
         revertDuration: 100,
         start:function(){$(this).stop(true,true)}
    });
-  
+
      $('.inventory-grid td').droppable({
          drop:function(event, ui){
              snapToMiddle(ui.draggable,$(this));
-         }     
+         }
      });
 })
 
